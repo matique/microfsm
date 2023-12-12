f2a7088bd24cb091d92cd90f1616c88b9f33fe93
dittmar <dittmar.krall@matique.de>
2023-12-12 09:38:16 +0100

    'v0.1.7'

--- lib/version.rb
+++ lib/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class MicroFSM
-  VERSION = "0.1.6" # 2023-04-25
+  VERSION = "0.1.7" # 2023-12-12
+  # VERSION = "0.1.6" # 2023-04-25
   # VERSION = "0.1.5" # 2022-12-22
   # VERSION = "0.1.3" # 2021-06-22
   # VERSION = "0.1.2" # 2021-02-10
