5a46296803da35c290f9b693143a3ece8d9e45ba
dittmar <dittmar.krall@matique.de>
2024-10-20 15:47:54 +0200

    'v0.1.10'

--- lib/microfsm/version.rb
+++ lib/microfsm/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class MicroFSM
-  VERSION = "0.1.9" # 2024-05-22
+  VERSION = "0.1.10" # 2024-10-20
+  # VERSION = "0.1.9" # 2024-05-22
   # VERSION = "0.1.7" # 2023-12-12
   # VERSION = "0.1.6" # 2023-04-25
   # VERSION = "0.1.5" # 2022-12-22
